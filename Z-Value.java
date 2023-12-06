e2a0c4015deecf1a6ab13b27bc7eeeb6b4f51659
Nikhil Wadhwa <101242510+NikhilWadhwa77@users.noreply.github.com>
2023-12-06 20:14:32 +0530

    Update Z-Value.java

--- Z-Value.java
+++ Z-Value.java
@@ -1,1 +1,35 @@
+package ADI.Algorithms;
 
+import java.util.*;
+
+public class zValue {
+    public static void main(String[] args) {
+        String pattern = "aa";
+        String text = "aaaa";
+
+        String str = pattern + "$" + text;
+        int[] arr = new int[str.length()];
+
+        int count = 0;
+//        i = pattern.length() + 1 because it makes less iterations
+        for(int i = pattern.length()+1; i < str.length(); i++){
+            while(i+count < str.length() && str.charAt(i+count) == str.charAt(count)){
+                count++;
+                arr[i] = count;
+            }
+            count = 0;
+
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+        int index;
+        for(int i = pattern.length() + 1; i < str.length(); i++){
+            if(arr[i] == pattern.length()){
+                index = i - pattern.length() - 1;
+                System.out.print(index + " ");
+            }
+        }
+
+    }
+}
